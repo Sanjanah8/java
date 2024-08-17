4bc117b2b59108288dbb79ef746331087e251806
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-08-17 10:20:49 +0530

    Update remove duplicates from array.java

--- Arrays/easy/remove duplicates from array.java
+++ Arrays/easy/remove duplicates from array.java
@@ -1,1 +1,22 @@
+import java.util.Arrays;
+
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 2, 3, 4, 4, 5};
+        int n = arr.length;
+
+        int[] temp = new int[n];
+        int j = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            if (arr[i] != arr[i + 1]) {
+                temp[j++] = arr[i];
+            }
+        }
+        temp[j++] = arr[n - 1];
+
+        int[] result = Arrays.copyOf(temp, j);
+        System.out.println("Array after removing duplicates: " + Arrays.toString(result));
+    }
+}
 
