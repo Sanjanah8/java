aec02dbb0a1c8ede1db7b9bb5c7cc33cd54f06ee
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-08-17 10:15:39 +0530

    Create sum of all elements in array.java

---
+++ Arrays/easy/sum of all elements in array.java
@@ -0,0 +1,10 @@
+public class ArraySum {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int sum = 0;
+        for (int num : arr) {
+            sum += num;
+        }
+        System.out.println("Sum of all elements: " + sum);
+    }
+}
