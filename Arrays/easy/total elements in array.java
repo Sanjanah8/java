697c3eec4d6abb3205e055176715ac706caa0c29
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-08-17 19:53:08 +0530

    Create total elements in array.java

---
+++ Arrays/easy/total elements in array.java
@@ -0,0 +1,6 @@
+public class ArrayLength {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        System.out.println("Total number of elements: " + arr.length);
+    }
+}
