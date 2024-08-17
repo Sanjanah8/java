5081ec4e8f4820847032e88b15dfdf985769c1f2
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-08-17 19:52:46 +0530

    Create average of array elements.java

---
+++ Arrays/easy/average of array elements.java
@@ -0,0 +1,11 @@
+public class AverageArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int sum = 0;
+        for (int num : arr) {
+            sum += num;
+        }
+        double average = (double) sum / arr.length;
+        System.out.println("Average of array elements: " + average);
+    }
+}
