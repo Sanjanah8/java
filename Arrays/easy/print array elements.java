18c9858d80343a5bdf9ef42d3707684b4188170c
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-08-17 10:40:05 +0530

    Create print array elements.java

---
+++ Arrays/easy/print array elements.java
@@ -0,0 +1,9 @@
+public class PrintArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        System.out.print("Array elements: ");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
