5338e5323dcb53ec0c8add4207975d6a5a8c7614
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-08-17 10:21:40 +0530

    Create merge two arrays.java

---
+++ Arrays/easy/merge two arrays.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class MergeArrays {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 3};
+        int[] arr2 = {4, 5, 6};
+
+        int[] mergedArray = new int[arr1.length + arr2.length];
+        System.arraycopy(arr1, 0, mergedArray, 0, arr1.length);
+        System.arraycopy(arr2, 0, mergedArray, arr1.length, arr2.length);
+
+        System.out.println("Merged array: " + Arrays.toString(mergedArray));
+    }
+}
