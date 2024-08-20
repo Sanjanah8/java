8ff64a75570cca3d58163f52bb0a4a0bf99dd965
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-08-20 18:27:16 +0530

    Create infix to postfix.java

---
+++ Data Structures/Stack/Easy/infix to postfix.java
@@ -0,0 +1,49 @@
+import java.util.Stack;
+
+public class InfixToPostfix {
+    public static void main(String[] args) {
+        String infix = "a+b*(c^d-e)^(f+g*h)-i";
+        String postfix = infixToPostfix(infix);
+        System.out.println("Postfix: " + postfix);
+    }
+
+    public static String infixToPostfix(String expression) {
+        StringBuilder result = new StringBuilder();
+        Stack<Character> stack = new Stack<>();
+        for (char c : expression.toCharArray()) {
+            if (Character.isLetterOrDigit(c)) {
+                result.append(c);
+            } else if (c == '(') {
+                stack.push(c);
+            } else if (c == ')') {
+                while (!stack.isEmpty() && stack.peek() != '(') {
+                    result.append(stack.pop());
+                }
+                stack.pop();
+            } else {
+                while (!stack.isEmpty() && precedence(c) <= precedence(stack.peek())) {
+                    result.append(stack.pop());
+                }
+                stack.push(c);
+            }
+        }
+        while (!stack.isEmpty()) {
+            result.append(stack.pop());
+        }
+        return result.toString();
+    }
+
+    public static int precedence(char ch) {
+        switch (ch) {
+            case '+':
+            case '-':
+                return 1;
+            case '*':
+            case '/':
+                return 2;
+            case '^':
+                return 3;
+        }
+        return -1;
+    }
+}
