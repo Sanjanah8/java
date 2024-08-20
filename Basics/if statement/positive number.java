e315a7d3b1eea64e734e63f749e19cf5b384a465
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-08-20 18:52:46 +0530

    Create positive number.java

---
+++ Basics/if statement/positive number.java
@@ -0,0 +1,8 @@
+public class CheckPositive {
+    public static void main(String[] args) {
+        int number = 10;
+        if (number > 0) {
+            System.out.println("The number is positive.");
+        }
+    }
+}
