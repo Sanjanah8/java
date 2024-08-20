abd96d392400a5826bfb0aeea21c27d203410a08
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-08-20 18:53:04 +0530

    Create even or odd.java

---
+++ Basics/if statement/even or odd.java
@@ -0,0 +1,10 @@
+public class CheckEvenOdd {
+    public static void main(String[] args) {
+        int number = 7;
+        if (number % 2 == 0) {
+            System.out.println("The number is even.");
+        } else {
+            System.out.println("The number is odd.");
+        }
+    }
+}
