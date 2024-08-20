e94e865a6ff4c9ac27ec16eefdd5d69566e0d363
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-08-20 18:54:03 +0530

    Create maximum of two numbers.java

---
+++ Basics/if statement/maximum of two numbers.java
@@ -0,0 +1,10 @@
+public class MaxOfTwo {
+    public static void main(String[] args) {
+        int a = 5, b = 10;
+        if (a > b) {
+            System.out.println("a is greater than b.");
+        } else {
+            System.out.println("b is greater than or equal to a.");
+        }
+    }
+}
