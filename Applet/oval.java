af5e4d5cc64633281ed4adbc1eb3190936eb0042
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-08-30 22:48:48 +0530

    Create oval.java

---
+++ Applet/oval.java
@@ -0,0 +1,8 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+
+public class OvalApplet extends Applet {
+    public void paint(Graphics g) {
+        g.drawOval(50, 50, 150, 100);
+    }
+}
