bd26f8553451e07ddf89e263de03c67325b5310a
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-08-30 23:01:06 +0530

    Create floyds triangle.java

---
+++ Pattern printing/floyds triangle.java
@@ -0,0 +1,12 @@
+public class FloydsTriangle {
+    public static void main(String[] args) {
+        int n = 5, num = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(num + " ");
+                num++;
+            }
+            System.out.println();
+        }
+    }
+}
