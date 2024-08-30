82e07f674da765576d40ae3c1bed4b022fb57582
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-08-30 23:03:37 +0530

    Create sandglass.java

---
+++ Pattern printing/sandglass.java
@@ -0,0 +1,23 @@
+public class SandglassPattern {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("  ");
+            }
+            for (int k = 0; k < n - i; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print("  ");
+            }
+            for (int k = 0; k <= i; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
