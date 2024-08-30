fce518fa12f10a4d539f27d446547aa7620c162f
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-08-30 23:04:50 +0530

    Create zigzag.java

---
+++ Pattern printing/zigzag.java
@@ -0,0 +1,16 @@
+public class ZigZagPattern {
+    public static void main(String[] args) {
+        int n = 9;
+
+        for (int i = 1; i <= 3; i++) {
+            for (int j = 1; j <= n; j++) {
+                if ((i + j) % 4 == 0 || (i == 2 && j % 4 == 0)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
