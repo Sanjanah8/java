af355ed10aced73859415f2f89ba2edc7302a411
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-08-30 22:48:30 +0530

    Create rectangle.java

---
+++ Applet/rectangle.java
@@ -0,0 +1,8 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+
+public class RectangleApplet extends Applet {
+    public void paint(Graphics g) {
+        g.drawRect(50, 50, 150, 100);
+    }
+}
