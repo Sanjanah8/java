2125b77c67ac957318dcec80891ea267c1ae11e0
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-08-30 22:59:41 +0530

    Create right pascals triangle.java

---
+++ Pattern printing/right pascals triangle.java
@@ -0,0 +1,17 @@
+public class RightPascalsTriangle {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
