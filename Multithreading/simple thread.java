bbac384e4653363c68a48586fa4e6a0a615871b0
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-08-30 22:33:00 +0530

    Create simple thread.java

---
+++ Multithreading/simple thread.java
@@ -0,0 +1,12 @@
+class MyThread extends Thread {
+    public void run() {
+        System.out.println("Thread is running");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        MyThread t1 = new MyThread();
+        t1.start();
+    }
+}
