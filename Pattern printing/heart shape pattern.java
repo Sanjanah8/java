73a81bee17c6e4058c29fab46bb77ad494c0431b
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-08-30 23:07:58 +0530

    Create heart shape pattern.java

---
+++ Pattern printing/heart shape pattern.java
@@ -0,0 +1,30 @@
+public class HeartShapePattern {
+    public static void main(String[] args) {
+        int n = 6;
+
+        for (int i = n / 2; i <= n; i += 2) {
+            for (int j = 1; j < n - i; j += 2) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = n; i >= 1; i--) {
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (i * 2) - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
