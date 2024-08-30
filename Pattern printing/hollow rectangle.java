346427c413239f5d832c11b162902472aa2b9fa7
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-08-30 23:00:20 +0530

    Create hollow rectangle.java

---
+++ Pattern printing/hollow rectangle.java
@@ -0,0 +1,15 @@
+public class HollowRectangle {
+    public static void main(String[] args) {
+        int rows = 5, cols = 7;
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= cols; j++) {
+                if (i == 1 || i == rows || j == 1 || j == cols) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
