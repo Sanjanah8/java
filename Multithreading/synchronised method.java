b99a8738a7c5796ac2246e633dec47636e6dfa05
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-08-30 22:39:02 +0530

    Create synchronised method.java

---
+++ Multithreading/synchronised method.java
@@ -0,0 +1,28 @@
+class Counter {
+    private int count = 0;
+
+    public synchronized void increment() {
+        count++;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        Counter counter = new Counter();
+
+        Thread t1 = new Thread(counter::increment);
+        Thread t2 = new Thread(counter::increment);
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        System.out.println("Final Count: " + counter.getCount());
+    }
+}
