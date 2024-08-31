3b12eefebd04073ae605c58df382ea3f56059e84
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-08-31 21:32:58 +0530

    Create largest of three numbers.java

---
+++ Basics/Easy and tricky/largest of three numbers.java
@@ -0,0 +1,30 @@
+// Title: Find the Largest of Three Numbers
+
+// This program finds the largest of three given numbers using nested if-else statements.
+
+import java.util.Scanner;
+
+public class LargestOfThree {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+        System.out.print("Enter third number: ");
+        int num3 = sc.nextInt();
+        
+        // Determine the largest number
+        int largest;
+        if (num1 >= num2 && num1 >= num3) {
+            largest = num1;
+        } else if (num2 >= num1 && num2 >= num3) {
+            largest = num2;
+        } else {
+            largest = num3;
+        }
+        
+        // Display the largest number
+        System.out.println("The largest number is: " + largest);
+    }
+}
