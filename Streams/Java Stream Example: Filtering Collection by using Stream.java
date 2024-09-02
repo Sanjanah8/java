7dd77be5b7d598d69c4f845eeb68472bcf4f5fe6
Sanjanah A <139737652+Sanjanah8@users.noreply.github.com>
2024-09-02 22:01:37 +0530

    Create  Java Stream Example: Filtering Collection by using Stream.java

---
+++ Streams/Java Stream Example: Filtering Collection by using Stream.java
@@ -0,0 +1,28 @@
+import java.util.*;  
+import java.util.stream.Collectors;  
+class Product{  
+    int id;  
+    String name;  
+    float price;  
+    public Product(int id, String name, float price) {  
+        this.id = id;  
+        this.name = name;  
+        this.price = price;  
+    }  
+}  
+public class JavaStreamExample {  
+    public static void main(String[] args) {  
+        List<Product> productsList = new ArrayList<Product>();  
+        //Adding Products  
+        productsList.add(new Product(1,"HP Laptop",25000f));  
+        productsList.add(new Product(2,"Dell Laptop",30000f));  
+        productsList.add(new Product(3,"Lenevo Laptop",28000f));  
+        productsList.add(new Product(4,"Sony Laptop",28000f));  
+        productsList.add(new Product(5,"Apple Laptop",90000f));  
+        List<Float> productPriceList2 =productsList.stream()  
+                                     .filter(p -> p.price > 30000)// filtering data  
+                                     .map(p->p.price)        // fetching price  
+                                     .collect(Collectors.toList()); // collecting as list  
+        System.out.println(productPriceList2);  
+    }  
+}  
